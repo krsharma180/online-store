f68f5e7e345b1f3e87bc14f0ccd8ad8f2a7c7b0c
krsharma180 <sharma.kartik180@gmail.com>
2020-10-10 17:28:44 +0530

    Add files via upload

---
+++ client/build/precache-manifest.807804c68c462c11ce95d5bc52713b4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9021399323fa4f0fda1686a25dc21aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61d9bf054aba656b8731",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "d99e7afa517ee3e7be01",
+    "url": "/static/css/main.ccdf3b5b.chunk.css"
+  },
+  {
+    "revision": "61d9bf054aba656b8731",
+    "url": "/static/js/2.5aad4177.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5aad4177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d99e7afa517ee3e7be01",
+    "url": "/static/js/main.12fb196d.chunk.js"
+  },
+  {
+    "revision": "6bc3485377616180338f",
+    "url": "/static/js/runtime-main.84821237.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
